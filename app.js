88615e0df23d3b8d16a777ab0d2017eba05f60fd
lautarodpr <lautarogarcia172@gmail.com>
2023-01-02 22:24:31 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.listen(3030, () => {
+    console.log('Servidor corriendo en https://localhost:3030');
+});
